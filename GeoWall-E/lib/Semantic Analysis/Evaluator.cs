7b2a2982216d9bef60175b048994372cface9e55
Vladimir Pinera <vladypv3@gmail.com>
2023-11-17 02:40:06 -0500

    Evaluator retorna lo que hay que pintar

--- GeoWall-E/lib/Semantic Analysis/Evaluator.cs
+++ GeoWall-E/lib/Semantic Analysis/Evaluator.cs
@@ -1,18 +1,46 @@
-
 namespace GeoWall_E;
 
 public class Evaluator
 {
     public List<Node> Root { get; set; }
+    public static Errors Errors { get; set; } = new Errors();
+    public static List<DeclaredVariable> VariableScope { get; set; } = new List<DeclaredVariable>();
 
     public Evaluator(List<Node> root)
     {
         Root = root;
     }
 
-    /*public List<Types> Evaluate()
+    public List<Types> Evaluate()
     {
-        return
+        List<Types> toDraw = new List<Types>();
+        foreach (var node in Root)
+        {
+            if (node is PointStatement point)
+            {
+                VariableScope.Add(new DeclaredVariable(point.Name.Text, ObjectTypes.Point, point.Color, point.IsSequence));
+            }
+
+            if (node is Draw draw)
+            {
+                if (draw.Expression is VariableExpression variable)
+                {
+                    var variableToDraw = VariableScope.Find(x => x.Name == variable.Name.Text);
+                    if (variableToDraw != null)
+                    {
+                        if (variableToDraw.Type == ObjectTypes.Point)
+                        {
+                            toDraw.Add(new Point(variableToDraw.Color ?? new Color(Colors.Black), variableToDraw.Name));
+                        }
+                    }
+                    else
+                    {
+                        Errors.AddError($"Variable {variable.Name.Text} not declared, Line: {variable.Name.Line}, Column: {variable.Name.Column}");
+                    }
+                }
+            }
+        }
+        return toDraw;
     }
-    */
+
 }
