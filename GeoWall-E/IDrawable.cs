6f94ac6fe0e268ae1f9d1ed3241926da4d90c2c1
Vladimir Pinera <vladypv3@gmail.com>
2023-12-10 09:29:13 -0500

    informe

--- GeoWall-E/IDrawable.cs
+++ GeoWall-E/IDrawable.cs
@@ -2,9 +2,6 @@ namespace GeoWall_E
 {
     public interface IDrawable
     {
-        // Esta interface la puedes quitar, depende de como vayas a implementar tu parte
         void Draw();
-
-        
     }
 }
